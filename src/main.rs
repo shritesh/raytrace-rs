a0e75b0ce29a2e6c225be45f4ebc1cfb87e49628
Shritesh Bhattarai <shr@ite.sh>
2020-05-08 07:53:41 -0500

    Chapter 5

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,29 @@ use image::ImageBuffer;
 use ray::Ray;
 use vec3::Vec3;
 
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - *center;
+    let a = r.direction.dot(&r.direction);
+    let b = 2.0 * oc.dot(&r.direction);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 pub fn ray_color(r: &Ray) -> Vec3 {
-    let unit_direction = r.direction.unit_vector();
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+    if hit_sphere(&Vec3(0.0, 0.0, -1.0), 0.5, r) {
+        Vec3(1.0, 0.0, 0.0)
+    } else {
+        let unit_direction = r.direction.unit_vector();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+    }
 }
 
 pub fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 384;
+    let image_height = (image_width as f64 / aspect_ratio) as u32;
 
     let origin = Vec3(0.0, 0.0, 0.0);
     let horizontal = Vec3(4.0, 0.0, 0.0);
