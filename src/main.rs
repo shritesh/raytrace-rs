a09b95e95e025cdc802cf7da140f3f473d5423d7
Shritesh Bhattarai <shr@ite.sh>
2020-05-07 22:59:41 -0500

    Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use image::{ImageBuffer, Rgb};
+mod vec3;
+
+use image::ImageBuffer;
+use vec3::Vec3;
 
 fn main() {
     let image_width = 256;
@@ -8,15 +11,12 @@ fn main() {
         let i = x;
         let j = image_height - y;
 
-        let r = i as f64 / (image_width - 1) as f64;
-        let g = j as f64 / (image_height - 1) as f64;
-        let b = 0.25;
-
-        Rgb([
-            (r * 255.999) as u8,
-            (g * 255.999) as u8,
-            (b * 255.999) as u8,
-        ])
+        Vec3(
+            i as f64 / (image_width - 1) as f64,
+            j as f64 / (image_height - 1) as f64,
+            0.25,
+        )
+        .into_rgb()
     })
     .save("image.png")
     .unwrap();
---
+++ src/vec3.rs
@@ -0,0 +1,93 @@
+use image::Rgb;
+use std::ops::{Add, Div, Mul, Neg, Sub};
+pub struct Vec3(pub f64, pub f64, pub f64);
+
+impl Vec3 {
+    pub fn x(&self) -> f64 {
+        self.0
+    }
+
+    pub fn y(&self) -> f64 {
+        self.1
+    }
+
+    pub fn z(&self) -> f64 {
+        self.2
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+    }
+
+    pub fn dot(&self, rhs: &Vec3) -> f64 {
+        self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2
+    }
+
+    pub fn cross(&self, rhs: &Vec3) -> Vec3 {
+        Vec3(
+            self.1 * rhs.2 - self.2 * rhs.1,
+            self.2 * rhs.0 - self.0 * rhs.2,
+            self.0 * rhs.1 - self.1 * rhs.0,
+        )
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        self / self.length()
+    }
+
+    pub fn into_rgb(&self) -> Rgb<u8> {
+        Rgb([
+            (self.0 * 255.999) as u8,
+            (self.1 * 255.999) as u8,
+            (self.2 * 255.999) as u8,
+        ])
+    }
+}
+
+impl Neg for &Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3(-self.0, -self.1, -self.2)
+    }
+}
+
+impl Add for &Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: &Vec3) -> Self::Output {
+        Vec3(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    }
+}
+
+impl Sub for &Vec3 {
+    type Output = Vec3;
+    fn sub(self, rhs: &Vec3) -> Self::Output {
+        Vec3(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
+    }
+}
+
+impl Mul for &Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: &Vec3) -> Self::Output {
+        Vec3(self.0 * rhs.0, self.1 * rhs.1, self.2 * rhs.2)
+    }
+}
+
+impl Mul<f64> for &Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
+impl Div<f64> for &Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: f64) -> Self::Output {
+        Vec3(self.0 / rhs, self.1 / rhs, self.2 / rhs)
+    }
+}
